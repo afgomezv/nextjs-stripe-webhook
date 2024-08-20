7ca7cc3a88387d4ff50e1f37108af1bdafc4b87b
afgomezv <afgomezvonline@gmail.com>
2024-08-20 09:10:22 -0500

    fix error webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -9,44 +9,41 @@ if(!stripeSecretKey){
     throw new Error('Stripe secret key is not defined in environment variables')
 }
 
-if(!endpointsSecret) {
-    throw new Error('Stripe webhook secret is not defined in environment variables')
-}
-
 const stripe = new Stripe(stripeSecretKey)
-const webhookSecret = endpointsSecret
-
 
 export async function POST(request: NextRequest){
     const body = await request.text()
     const headersList = headers()
-    const signature = headersList.get('Stripe-Signature')
+    const sig = headersList.get('stripe-signature')
+
+    if(!endpointsSecret) {
+        throw new Error('Stripe endpointSecret is not defined in environment variables')
+    }
+
+    if(!sig){
+        throw new Error('Sinature undefided')
+    }
+
     let event
-
-    if( !signature ) return
-
+    
     try {
-        event = stripe.webhooks.constructEvent(body, signature, webhookSecret)
+        event = stripe.webhooks.constructEvent(body, sig, endpointsSecret)
     } catch (error: any) {
         console.log(error);
-        return NextResponse.json({error: error.message}, {status: 400})
+        return NextResponse.json({error: error.mensaje}, {status: 400})
     }
 
     switch(event.type){
-        case 'checkout.session.completed':
+        case "checkout.session.completed":
             const checkoutSessionCompleted = event.data.object
-            // Guardar en una base de datos
-            console.log('consultando producto con id', checkoutSessionCompleted.metadata);
-            
+            //* Guardar en base de datos
 
-            // Enviar un correo
-            console.log({checkoutSessionCompleted});
-            break
+            //* Enviar un correo
+            console.log({checkoutSessionCompleted})
+            break;
         default:
-            console.log(`Event no manejado ${event.type}`);
-            
-        }
-        
-
-    return NextResponse.json('Recibiendo webhook')
+            console.log(`Evento no manejado ${event.type}`);
+    }
+    
+    return NextResponse.json(null, {status: 200})
 }
